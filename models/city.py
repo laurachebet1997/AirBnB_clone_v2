e468e0028573ca9dc0d6dfe252d6fb8e9f1ce571
cedouiri <cedouiri@gmail.com>
2020-04-02 21:31:03 +0000

    update City

--- models/city.py
+++ models/city.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
-from os import environ
-from uuid import uuid4
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
+from os import environ
+from uuid import uuid4
 
 
 if "HBNB_TYPE_STORAGE" in environ.keys(
 ) and environ["HBNB_TYPE_STORAGE"] == "db":
     class City(BaseModel, Base):
-        """This is the class for City
-        Attributes:
-        state_id: The state id
-        name: input name
-        """
+        '''
+        This is the class for City Attributes
+        '''
         __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
@@ -22,14 +20,12 @@ if "HBNB_TYPE_STORAGE" in environ.keys(
 
         def __init__(self, **kwargs):
             setattr(self, "id", str(uuid4()))
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+            for i, j in kwargs.items():
+                setattr(self, i, j)
 else:
     class City(BaseModel):
-        """This is the class for City
-        Attributes:
-        state_id: The state id
-        name: input name
-        """
+        '''
+        This is the class for City
+        '''
         state_id = ""
         name = ""
