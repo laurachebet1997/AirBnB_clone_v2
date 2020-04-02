1c01140ba1d6a1f8aef3b402c29a59c9b5c9d77d
afinesami <samibenomar84@gmail.com>
2020-04-02 17:07:43 +0200

    [add] db storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This is the file storage class for AirBnB"""
+import json
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.exc import InvalidRequestError
+
+
+class DBStorage:
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+    __engine = None
+    __session = None
+    
