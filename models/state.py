fe23e41c185a4e491ee8c5683b43036b18e44387
cedouiri <cedouiri@gmail.com>
2020-04-02 21:40:24 +0000

    update State

--- models/state.py
+++ models/state.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
-from os import environ
-import models
-from uuid import uuid4
 from models.base_model import Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
+import models
+from uuid import uuid4
 
 
 if "HBNB_TYPE_STORAGE" in environ.keys(
 ) and environ["HBNB_TYPE_STORAGE"] == "db":
     class State(BaseModel, Base):
-        """
-        This is the state class
-        """
+        '''
+        state class
+        '''
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
 
         def __init__(self, **kwargs):
             setattr(self, "id", str(uuid4()))
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+            for i, j in kwargs.items():
+                setattr(self, i, j)
 else:
     class State(BaseModel):
-        """
-        this is the state class
-        """
+        '''
+        state class
+        '''
         name = ""
 
         @property
         def cities(self):
-            all_cities = models.storage.all(City)
-            lista = []
-            keys = all_cities.items()
+            all_city = models.storage.all(City)
+            liste = []
+            keys = all_city.items()
             for i, j in keys:
                 if "City" == i[0:4] and j.state_id == self.id:
-                    lista.append(j)
-            return(lista)
+                    liste.append(j)
+            return(liste)
