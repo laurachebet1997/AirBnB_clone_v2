2b568ce906f2300e98faedb73a094ad7c7da3f67
cedouiri <cedouiri@gmail.com>
2020-04-02 22:41:22 +0000

    add condition

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,10 @@
 from models.base_model import BaseModel
 
 s = "HBNB_TYPE_STORAGE"
-class Amenity(BaseModel):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    name = ""
+if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Amenity(BaseModel):
+        """This is the class for Amenity
+        Attributes:
+            name: input name
+        """
+        name = ""
