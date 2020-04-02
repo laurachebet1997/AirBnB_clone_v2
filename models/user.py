1a5753136e63becb0d487b6a25e8cc148cc2c1ab
afinesami <samibenomar84@gmail.com>
2020-04-02 17:37:59 +0200

    [update] user

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     password: password for you login
     first_name: first name
     last_name: last name
-    """    
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
