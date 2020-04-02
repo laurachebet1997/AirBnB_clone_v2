6fde46f65439b6be50ad8cc0fe69d51ec4eb47cf
cedouiri <cedouiri@gmail.com>
2020-04-02 22:39:38 +0000

    add variable

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,7 @@
 """This is the amenity class"""
 from models.base_model import BaseModel
 
-
+s = "HBNB_TYPE_STORAGE"
 class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
