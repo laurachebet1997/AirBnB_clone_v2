5cbce59d9b8759816a316224ee891bbc6c5fe468
cedouiri <cedouiri@gmail.com>
2020-04-02 22:43:37 +0000

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,18 @@ from models.base_model import BaseModel
 
 s = "HBNB_TYPE_STORAGE"
 if s in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class Amenity(BaseModel, Base):
+        """
+        This is the state class
+        """
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
     class Amenity(BaseModel):
         """This is the class for Amenity
         Attributes:
