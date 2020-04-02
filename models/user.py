cf4aa6cfd5cb8ad483ee8389cad2c20d55a25fa1
afinesami <samibenomar84@gmail.com>
2020-04-02 17:24:41 +0200

    [update] user

--- models/user.py
+++ models/user.py
@@ -3,22 +3,41 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from uuid import uuid4
+from os import environ
 
 
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
+if "HBNB_TYPE_STORAGE" in environ.keys() and environ["HBNB_TYPE_STORAGE"] == "db":
+    class User(BaseModel, Base):
+        """This is the class for user
+        Attributes:
         email: email address
         password: password for you login
         first_name: first name
         last_name: last name
-    """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    reviews = relationship('Review', cascade='save-update, delete, merge',
-                           backref='user')
-    places = relationship('Place', cascade='save-update, delete, merge',
-                          backref='user')
+        """
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class User(BaseModel):
+        """This is the class for user
+        Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+        """
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
