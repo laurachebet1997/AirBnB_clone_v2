c86837b38b03e085fadfe32cfbaa1959e011091f
afinesami <samibenomar84@gmail.com>
2020-04-02 17:03:10 +0200

    state city

--- models/city.py
+++ models/city.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from os import environ
+from uuid import uuid4
+from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """This is the class for City
-    Attributes:
+if "HBNB_TYPE_STORAGE" in environ.keys(
+) and environ["HBNB_TYPE_STORAGE"] == "db":
+    class City(BaseModel, Base):
+        """This is the class for City
+        Attributes:
         state_id: The state id
         name: input name
-    """
-    state_id = ""
-    name = ""
+        """
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all,delete")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class City(BaseModel):
+        """This is the class for City
+        Attributes:
+        state_id: The state id
+        name: input name
+        """
+        state_id = ""
+        name = ""
--- models/state.py
+++ models/state.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from os import environ
+import models
+from uuid import uuid4
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    name = ""
+if "HBNB_TYPE_STORAGE" in environ.keys(
+) and environ["HBNB_TYPE_STORAGE"] == "db":
+    class State(BaseModel, Base):
+        """
+        This is the state class
+        """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class State(BaseModel):
+        """
+        this is the state class
+        """
+        name = ""
+
+        @property
+        def cities(self):
+            all_cities = models.storage.all(City)
+            lista = []
+            keys = all_cities.items()
+            for i, j in keys:
+                if "City" == i[0:4] and j.state_id == self.id:
+                    lista.append(j)
+            return(lista)
