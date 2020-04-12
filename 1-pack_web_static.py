929ad2e910deee07ddb05ae52491f03c4e2e703d
afinesami <samibenomar84@gmail.com>
2020-04-12 19:03:22 +0100

    pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Script Fabric that generates a .tgz archive from the contents of the web_static """
+from fabric.api import local
+
+
+def do_pack():
+	""" Function return the archive path"""
+	return None
