190815898b774fe09487ca7951fde104130ccfaa
laurachebet1997 <rosechebet19971997@gmail.com>
2022-11-24 22:38:21 +0300

    v2

---
+++ 100-clean_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+keep it clean module
+"""
+import os
+from fabric.api import *
+
+env.hosts = ["52.201.168.242", "18.207.1.214"]
+
+
+def do_clean(number=0):
+    """
+    Deletes out of date archives
+    """
+    if int(number) == 0:
+        number = 1
+    else:
+        number = int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
