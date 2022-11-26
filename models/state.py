0865802b71ef87a0e91af6fe6a72a26960bfc36a
laurachebet1997 <rosechebet19971997@gmail.com>
2022-11-26 10:38:45 +0300

    sql

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # This is the state class
 from models.base_model import BaseModel, Base
-from models import *
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship, backref
 from os import getenv
